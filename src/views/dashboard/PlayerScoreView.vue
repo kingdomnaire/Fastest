bb3a7a1f2980bddbee8cdede6b1f25e9b5a8a0dc
Emmanuel Amadi <emmanuelamadi111@gmail.com>
2023-12-02 17:13:02 +0100

    update

--- src/views/dashboard/PlayerScoreView.vue
+++ src/views/dashboard/PlayerScoreView.vue
@@ -2,13 +2,13 @@
   <div>
     <NavbarView />
     <div class="w-screen h-full absolute z-30 bg-opacity-80 flex justify-center">
-      <!-- Check if players array is empty -->
-      <div v-if="players.length === 0" class="">
-        <p class="text-gray-400 font-bold text-center">Data will show after the game</p>
-        <p class="text-gray-400 font-bold text-center">Hello {{ login.firstName }}! the game is locked, you will be notified soon.</p>
+      <div v-if="players.length === 0" class="text-center">
+        <p class="text-gray-400 font-bold">Data will show after the game</p>
+        <p class="text-gray-400 font-bold">Hello {{ login.firstName }}! The game is locked; you will be notified soon.</p>
       </div>
       <template v-else>
-        <table>
+        <table class="table-auto">
+          <!-- Add styling classes specific to your design system -->
           <thead>
             <tr class="bg-blue-600 h-10 text-gray-200">
               <th class="pr-10 pl-2">Player Name</th>
@@ -16,8 +16,8 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="player in players" :key="player.id" class="bg-blue-900  h-10 text-gray-200 font-bold">
-              <td class="pr-40 pl-4">{{ player.user.firstName + ' ' + player.user.lastName }}</td>
+            <tr v-for="player in players" :key="player.id" class="bg-blue-900 h-10 text-gray-200 font-bold">
+              <td class="pr-40 pl-4">{{ getPlayerName(player.user) }}</td>
               <td class="pr-4 pl-2">{{ player.time }}</td>
             </tr>
           </tbody>
@@ -26,35 +26,41 @@
     </div>
   </div>
 </template>
-  
-  <script>
-  import axios from 'axios';
-  import NavbarView from './NavbarView.vue';
-  
-  export default {
-    components: {
-      NavbarView,
-    },
-    data() {
-      return {
-        players: [],
-        login: {}, // Initialize the login object with an empty object
-      };
-    },
-    async mounted() {
-      // Retrieve the login details from localStorage and store them in the data property
-    const loginData = localStorage.getItem('login');
-    if (loginData) {
-      this.login = JSON.parse(loginData);
-    }
-      try {
-        const response = await axios.get('https://quizzes-bmo0.onrender.com/fingers');
-        this.players = response.data;
-        console.log(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    },
-  };
-  </script>
 
+<script>
+import axios from 'axios';
+import NavbarView from './NavbarView.vue';
+
+export default {
+  components: {
+    NavbarView,
+  },
+  data() {
+    return {
+      players: [],
+      login: {}, // Initialize the login object with an empty object
+    };
+  },
+  methods: {
+    getPlayerName(user) {
+      // Add logic to get the player name based on user data
+      return `${user.firstName} ${user.lastName}`;
+    },
+  },
+  async mounted() {
+    try {
+      const loginData = localStorage.getItem('login');
+      if (loginData) {
+        this.login = JSON.parse(loginData);
+      }
+
+      const response = await axios.get('https://quizzes-bmo0.onrender.com/fingers');
+      this.players = response.data;
+      console.log(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      // You might want to provide more detailed error handling here
+    }
+  },
+};
+</script>
